5ee2198cb3368bd006e9369cfa8e5ac9b0c72ded
Maciej Kaczor <hangeyes@gmail.com>
2015-08-31 21:07:00 +0200

    First and final commit

---
+++ js/controllers.js
@@ -0,0 +1,88 @@
+var mapApp = angular.module("mapApp", ['chart.js']);
+mapApp.controller("infoCtrl", function($scope, $http) {
+	var geojson;
+	$http.get("http://hangeyes.pl/mapApp/geoJSON.json").success(function(response) {
+		$scope.objects = response.features;
+		$scope.properties = response.features[0].properties;
+		var map = L.map('map').setView([
+              52.4102835766679,
+              16.8758386862567
+            ], 14);
+	L.tileLayer('http://api.gis-support.pl/v1/maptile/0062ea45e2b9e7957211ef97a9791b71/tiles/inwestor/webmercator/{z}/{x}/{y}.png', {
+  		attribution: "Dane © OpenStreetMap.org, hosting i grafika GIS Support ",
+  		maxZoom: 18
+  }).addTo(map);
+	geojson = L.geoJson($scope.objects, {
+		style: {
+    	"color": "#ff7800",
+    	"weight": 5,
+    	"opacity": 0.65
+		},
+		onEachFeature: onEachFeature
+	} ).addTo(map);
+	});
+	
+	$scope.labels = [];
+  	$scope.series = ['Seria A'];
+	
+  	$scope.data = [];
+	$scope.selectedOption = 0;
+	$scope.changeOption = function() {
+		var sum = 0;
+		var labelSet = [];
+		var dataSet = [];
+		angular.forEach($scope.objects, function(value, key) {
+			sum += parseInt(value.properties[$scope.selectedItem]);
+			labelSet.push(value.id);
+			dataSet.push(parseFloat(value.properties[$scope.selectedItem]));	
+		});
+		$scope.sum = sum;
+		$scope.avg = sum/$scope.objects.length;
+		$scope.data = [dataSet];
+		$scope.labels = labelSet;
+	};
+	
+	function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+}
+
+	function resetHighlight(e) {
+    	geojson.resetStyle(e.target);
+	}
+
+	function onClick(e) {
+		var dataSet = [];
+		$scope.labels = [];
+		angular.forEach($scope.objects, function(value, key) {
+			if(e.target.feature.id == value.id) {
+				dataSet.push(parseFloat(value.properties[$scope.selectedItem]));
+				
+			} else {
+				dataSet.push(0);
+			}
+			$scope.labels.push(value.id);
+		});
+		$scope.data = [dataSet];
+		$scope.$apply();
+	}
+	function onEachFeature(feature, layer) {
+	    layer.on({
+	        mouseover: highlightFeature,
+	        mouseout: resetHighlight,
+	        click: onClick
+	    });
+	}
+});
+
+
